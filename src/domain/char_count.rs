25a4b24697826fdbb3a461548c9dfdd290d02f78
Nahum Sá <nahumsaa@gmail.com>
2023-11-04 18:57:01 -0300

    add character_count function

--- src/domain/char_count.rs
+++ src/domain/char_count.rs
@@ -1,25 +1,46 @@
 use std::collections::HashMap;
 
 pub fn character_count(input: &str) -> HashMap<char, u32> {
-    let output = HashMap::new();
-    output
+    let mut char_count: HashMap<char, u32> = HashMap::new();
+    for char in input.chars() {
+        let count = char_count.entry(char).or_insert(0);
+        *count += 1;
+    }
+    char_count
 }
 
 #[cfg(test)]
 mod tests {
     use super::character_count;
     use std::collections::HashMap;
+
     #[test]
     fn test_character_count() {
         let input = "hello world";
-        let mut output = HashMap::new();
-        output.insert('h', 1);
-        output.insert('e', 1);
-        output.insert('l', 3);
-        output.insert('o', 2);
-        output.insert('w', 1);
-        output.insert('r', 1);
-        output.insert('d', 1);
+        let output = HashMap::from([
+            ('h', 1),
+            ('e', 1),
+            ('l', 3),
+            (' ', 1),
+            ('o', 2),
+            ('w', 1),
+            ('r', 1),
+            ('d', 1),
+        ]);
+        assert_eq!(character_count(input), output);
+    }
+
+    #[test]
+    fn test_character_count_only_one_letter() {
+        let input = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+        let output = HashMap::from([('e', 40)]);
+        assert_eq!(character_count(input), output);
+    }
+
+    #[test]
+    fn test_character_count_empty_input() {
+        let input = "";
+        let output = HashMap::from([]);
         assert_eq!(character_count(input), output);
     }
 }
