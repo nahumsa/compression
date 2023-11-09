349b35c7552bf9a7ad53237567c6584519545eb0
Nahum Sá <nahumsaa@gmail.com>
2023-11-09 09:02:41 -0300

    restructured tree to be more specific to huffman trees

--- src/domain/tree.rs
+++ src/domain/tree.rs
@@ -1,23 +1,36 @@
-use std::{cell::RefCell, collections::VecDeque, rc::Rc};
+use std::collections::VecDeque;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct HuffmanNode {
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct HuffmanNode<T> {
     pub weight: u8,
-    pub char: char,
+    pub char: T,
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Tree<T> {
-    pub node: T,
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Tree<T: Clone + Eq> {
+    pub node: HuffmanNode<T>,
+    pub weight: u8,
     pub left: Option<Box<Tree<T>>>,
     pub right: Option<Box<Tree<T>>>,
 }
 
-impl<T> Tree<T> {
-    pub fn new(node: T) -> Self {
+impl<T: Clone + Eq> Ord for Tree<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.weight.cmp(&other.weight)
+    }
+}
+
+impl<T: Clone + Eq> PartialOrd for Tree<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: Copy + Eq> Tree<T> {
+    pub fn new(node: HuffmanNode<T>, weight: u8) -> Self {
         Tree {
-            node: node,
             node,
+            weight,
             left: None,
             right: None,
         }
@@ -32,6 +45,11 @@ impl<T> Tree<T> {
         self.right = Some(Box::new(node));
         self
     }
+
+    pub fn from_vec(values: Vec<HuffmanNode<T>>) -> Self {
+        let (first_element, rest) = values.split_first().unwrap();
+        Tree::new(*first_element, first_element.weight)
+    }
 }
 
 #[cfg(test)]
@@ -40,16 +58,23 @@ mod tests {
 
     #[test]
     fn test_new_tree() {
-        let tree = Tree::new(HuffmanNode {
-            weight: 0,
-            char: 'a',
-        });
-        let expect = HuffmanNode {
+        let tree = Tree::new(
+            HuffmanNode {
+                weight: 0,
+                char: 'a',
+            },
+            0,
+        );
+
+        let expect_node = HuffmanNode {
             weight: 0,
             char: 'a',
         };
 
-        assert_eq!(expect, tree.node);
+        let expect_weight = 0;
+
+        assert_eq!(expect_node, tree.node);
+        assert_eq!(expect_weight, tree.weight);
     }
 
     #[test]
@@ -64,20 +89,28 @@ mod tests {
             char: 'b',
         };
 
-        let tree = Tree::new(HuffmanNode {
-            weight: 0,
-            char: 'a',
-        })
-        .left(Tree::new(HuffmanNode {
-            weight: 0,
-            char: 'b',
-        }));
+        let tree = Tree::new(
+            HuffmanNode {
+                weight: 0,
+                char: 'a',
+            },
+            0,
+        )
+        .left(Tree::new(
+            HuffmanNode {
+                weight: 0,
+                char: 'b',
+            },
+            1,
+        ));
 
         if let Some(node) = tree.left {
             assert_eq!(expect_left, node.node);
+            assert_eq!(1, node.weight);
         }
 
         assert_eq!(expect_root, tree.node);
+        assert_eq!(0, tree.weight);
     }
 
     #[test]
@@ -92,19 +125,28 @@ mod tests {
             char: 'b',
         };
 
-        let tree = Tree::new(HuffmanNode {
-            weight: 0,
-            char: 'a',
-        })
-        .right(Tree::new(HuffmanNode {
-            weight: 0,
-            char: 'b',
-        }));
+        let tree = Tree::new(
+            HuffmanNode {
+                weight: 0,
+                char: 'a',
+            },
+            0,
+        )
+        .right(Tree::new(
+            HuffmanNode {
+                weight: 0,
+                char: 'b',
+            },
+            1,
+        ));
 
         if let Some(node) = tree.right {
             assert_eq!(expect_right, node.node);
+            assert_eq!(1, node.weight);
         }
 
         assert_eq!(expect_root, tree.node);
+        assert_eq!(0, tree.weight);
     }
+    //TODO: add test to insert
 }
